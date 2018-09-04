7ef35e03eebd3af4817da52d528c6c12a4d9d1e3
xiaobin <359584204gg@gmail.com>
2018-09-04 08:56:57 +0800

    主体框架

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  title: 'docs',  // 设置网站标题
+  dest: './dist',    // 设置输出目录
+  base: '/blogsite/', // 设置站点根路径
+  repo: 'https://github.com/xiaobinlove/blogsite', // 添加 github 链接
+  serviceWorker: true,
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Blog', link: '/blog/' },
+      {
+        text: 'Javascript',
+        items: [
+          { text: 'es5', link: '/javascript/es5/' },
+          { text: 'es6', link: '/javascript/es6/' }
+        ]
+      },
+      { text: 'External', link: 'https://google.com' }
+    ],
+    sidebar: {
+      '/javascript/es5/': genSidebarConfigES5('es5'),
+      '/blog/': genSidebarConfigBlog('es5'),
+    }
+  }
+}
+function genSidebarConfigES5 (title) {
+  return [
+    {
+      title,
+      collapsable: false,
+      children: [
+        '',
+        'class1',
+        'class2',
+        'class3'
+      ]
+    }
+  ]
+}
+function genSidebarConfigBlog (title) {
+  return [
+    {
+      title,
+      collapsable: false,
+      children: [
+        '',
+        'class1',
+        'class2',
+        'class3'
+      ]
+    }
+  ]
+}
---
+++ docs/README.md
@@ -0,0 +1,3 @@
+# 还没想好
+<el-button>案例</el-button>
+<hello/>
